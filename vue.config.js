af76fbfff44deb197837782cb1341a12d7a5207c
Anthony Gore <anthony@magnum.technology>
2019-03-14 14:31:05 +0800

    .

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  chainWebpack: config => {
+    config
+      .entry("app")
+      .clear()
+      .add("./client/main.js")
+      .end();
+    config.resolve.alias
+      .set("@", path.join(__dirname, "./client"));
+  }
+};
