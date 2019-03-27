866617773df038a73a5b9d08345a94d9232b9f32
Anthony Gore <anthony@magnum.technology>
2019-03-27 17:33:41 +0800

    .

--- server/index.js
+++ server/index.js
@@ -4,6 +4,28 @@ const path = require("path");
 require("dotenv").config({
   path: path.join(__dirname, "..", ".env.server")
 });
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/printbay", {
+  useNewUrlParser: true, useCreateIndex: true
+});
+
+const Item = mongoose.model("Item", {
+  title: {
+    type: String
+  },
+  artist: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  year: {
+    type: Number
+  },
+  price: {
+    type: Number
+  }
+});
 
 app.use("/items", require("./routes/items"));
 
