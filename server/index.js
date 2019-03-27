bd48a907b7186182f667f33402d57ad844d5c6b4
Anthony Gore <anthony@magnum.technology>
2019-03-27 12:47:31 +0800

    .

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ require("dotenv").config({
   path: path.join(__dirname, "..", ".env.server")
 });
 
+app.get("/items", (req, res) => {
+  res.send({ message: "It works!" });
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
