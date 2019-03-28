20c4b1fe5dbc2d257973f31c276862a5fe2e963b
Anthony Gore <anthony@magnum.technology>
2019-03-28 16:04:28 +0800

    .

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,8 +1,15 @@
+const Item = require("../models/Item");
+
 exports.list = (req, res) => {
   res.send({ message: "It works!" });
 };
 
-exports.create = (req, res) => {
-  console.log(req.body);
-  res.send({ message: "Received POST "});
+exports.create = async (req, res) => {
+  const item = new Item(req.body);
+  try {
+    const doc = await item.save();
+    res.send({ item: doc });
+  } catch (err) {
+    res.status(400).send();
+  }
 };
