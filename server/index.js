83276a3a1bad1ddbf66fc2f69400456d112bbb7c
Anthony Gore <anthony@magnum.technology>
2019-03-28 15:32:24 +0800

    .

--- server/index.js
+++ server/index.js
@@ -8,54 +8,6 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/printbay", {
   useNewUrlParser: true, useCreateIndex: true
 });
-const validator = require("validator");
-
-const Item = mongoose.model("Item", {
-  title: {
-    type: String,
-    require: true,
-    trim: true
-  },
-  artist: {
-    type: String,
-    trim: true
-  },
-  image: {
-    type: String,
-    trim: true
-  },
-  year: {
-    type: Number
-  },
-  price: {
-    type: Number
-  }
-});
-
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    validate: {
-      validator: validator.isEmail,
-      message: "{VALUE} is not a valid email"
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    trim: true
-  }
-});
 
 app.use("/items", require("./routes/items"));
 
---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    trim: true
+  }
+});
