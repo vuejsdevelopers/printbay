fb8c16e63bf21252e367d5426e1f26544e58cf38
Anthony Gore <anthony@magnum.technology>
2019-04-05 12:17:16 +0800

    .

--- tests/unit/server/item.test.js
+++ tests/unit/server/item.test.js
@@ -1,5 +1,12 @@
+const expect = require("expect");
+
 const add = (x, y) => x + y;
 
-it("should add two numbers", () => {
-
-});
+it(
+  "should add two numbers",
+  async () => new Promise(resolve => {
+    const result = add(2, 3);
+    expect(result).toBe(5);
+    resolve();
+  })
+);
