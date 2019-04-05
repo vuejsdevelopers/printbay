401eb70f50ff9b78664463c9c5d5b7412c99fcce
Anthony Gore <anthony@magnum.technology>
2019-04-05 12:57:56 +0800

    .

--- tests/unit/server/item.test.js
+++ tests/unit/server/item.test.js
@@ -1,8 +1,14 @@
 const expect = require("expect");
 const app = require("../../../server");
+const request = require("supertest");
 
 describe("POST /items", () => {
   it("should create a new item", async () => {
-    expect(true).toBe(true);
+    const body = { title: "Test title" };
+    const res = await request(app)
+      .post("/items")
+      .send(body)
+      .expect(200);
+    expect(res.body.item.title).toBe(body.title);
   });
 });
