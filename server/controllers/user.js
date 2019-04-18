8cf020e3067ff39fe484e03f54dd1627269e06cf
Anthony Gore <anthony@magnum.technology>
2019-04-18 13:01:19 +0800

    no message

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,10 @@ exports.create = async (req, res) => {
   const user = new Users(req.body);
   try {
     const doc = await user.save();
-    res.send({ user: doc });
+    const token = await doc.generateAuthToken();
+    res
+      .header("authorization", `Bearer ${token}`)
+      .send({ user: doc });
   } catch (err) {
     res.status(400).send(err);
   }
