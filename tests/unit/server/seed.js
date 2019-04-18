e25322bbdb7ca70105d01e23ab1de448f8f9fab2
Anthony Gore <anthony@magnum.technology>
2019-04-18 15:42:56 +0800

    no message

--- tests/unit/server/seed.js
+++ tests/unit/server/seed.js
@@ -1,5 +1,11 @@
 const Item = require("../../../server/models/Item");
+const User = require("../../../server/models/User");
 const { ObjectId } = require("mongodb");
+const faker = require("faker");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({
+  path: "../../../../.env.server"
+});
 
 const seedItems = [
   {
@@ -12,9 +18,33 @@ const seedItems = [
   }
 ];
 
+const userOneId = new ObjectId();
+const userTwoId = new ObjectId();
+
+const seedUsers = [
+  {
+    _id: userOneId,
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET).toString()
+  },
+  {
+    _id: userTwoId,
+    name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  }
+];
+
 const populateItems = async () => {
   await Item.deleteMany();
   await Item.insertMany(seedItems);
 };
 
-module.exports = { seedItems, populateItems };
+const populateUsers = async () => {
+  await User.deleteMany();
+  await User.insertMany(seedUsers);
+};
+
+module.exports = { seedItems, populateItems, seedUsers, populateUsers };
