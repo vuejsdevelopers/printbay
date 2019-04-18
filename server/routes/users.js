d638dcb41096e86b2466d3f1883cd58874ac1523
Anthony Gore <anthony@magnum.technology>
2019-04-18 11:54:12 +0800

    no message

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const UsersController = require("../controllers/users");
+const UsersController = require("../controllers/user");
 
 module.exports = router;
