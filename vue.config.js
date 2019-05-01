f48df95bc976e521550bfb436cda466035eb61f1
Anthony Gore <anthony@vuejsdevelopers.com>
2019-05-01 18:40:59 +0700

    no message

--- vue.config.js
+++ vue.config.js
@@ -8,10 +8,19 @@ module.exports = {
       .add("./client/main.js")
       .end();
     config.resolve.alias
-      .set("@", path.join(__dirname, "./client"));
+      .set("@", path.join(__dirname, "./client"))
+      .set("@components", path.join(__dirname, "client", "components"))
+      .set("@assets", path.join(__dirname, "client", "assets"))
+      .set("@views", path.join(__dirname, "client", "views"));
   },
   devServer: {
     port: process.env.PORT,
-    proxy: `http://localhost:${process.env.SERVER_PORT}`
+    proxy: `http://localhost:${process.env.SERVER_PORT}`,
+    host: "0.0.0.0",
+    public: "10.10.30.30",
+    watchOptions: {
+      poll: 500,
+      ignored: [/node_modules/, /scripts/, /server/]
+    }
   }
 };
