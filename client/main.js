8305ebb85bd40bade23541f6368548752247f33d
Anthony Gore <anthony@vuejsdevelopers.com>
2019-05-29 12:07:35 +0700

    no message

--- client/main.js
+++ client/main.js
@@ -7,6 +7,10 @@ import "vuetify/dist/vuetify.min.css";
 import "@assets/main.styl";
 import VueRouter from "vue-router";
 import Vuex from "vuex";
+import VueAuth from "@websanova/vue-auth";
+import VueAuthOptions from "./auth";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
 Vue.router = router;
@@ -14,6 +18,8 @@ Vue.router = router;
 Vue.use(Vuetify);
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(VueAxios, axios);
+Vue.use(VueAuth, VueAuthOptions);
 
 new Vue({
   router,
