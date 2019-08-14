3c8782475312430e08312fa2df2ad7772e3bb085
Anthony Gore <anthony@vuejsdevelopers.com>
2019-08-14 16:18:48 +0700

    no message

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,17 @@
 module.exports = {
   "add item to cart": browser => {
+    let url;
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
+      .waitForElementVisible(".item-summaries", 5000)
+      .getAttribute(
+        ".item-summaries > :first-child > a",
+        "href",
+        result => { url = result.value; }
+      )
       .click(".item-summaries > :first-child")
+      .waitForElementVisible(".item", 5000)
+      .perform(() => browser.assert.urlEquals(url))
       .end();
   }
 };
