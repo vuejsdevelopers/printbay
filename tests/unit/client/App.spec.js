3830dcb534d8cd898e75f81c6d4a594c7347794b
Anthony Gore <anthony@vuejsdevelopers.com>
2019-09-19 10:57:13 +0700

    no message

---
+++ tests/unit/client/App.spec.js
@@ -0,0 +1,15 @@
+import App from "@/App";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import VueRouter from "vue-router";
+import router from "@/router";
+import Vuetify from "vuetify";
+import Vue from "vue";
+Vue.use(Vuetify);
+
+describe("App.vue", () => {
+  it("should render correctly", () => {
+    const localVue = createLocalVue();
+    localVue.use(VueRouter);
+    const wrapper = shallowMount(App, { localVue, router });
+  });
+});
