ba1bd8d975fb2eae9daecb9c3d70eae0a08d840b
Mehmet Bulut <mail@mehmetbulut.de>
2020-12-04 17:33:25 +0100

    Pass mount options to container

--- run-dev-env.sh
+++ run-dev-env.sh
@@ -2,11 +2,38 @@
 
 set -euo pipefail
 
+function usage {
+    echo "Usage: $0 [OPTIONS] [IMAGE_NAME]"
+    echo ""
+    echo "Options"
+    echo " -h, --help         Display this message"
+    echo " -c, --cached       Mount volume with 'cached' option"
+    echo " -d, --delegated    Mount volume with 'delegated' option"
+}
+
+MOUNT_OPT=
 DOCKER_IMAGE=printbay-dev
-
-[[ $# -eq 0 ]] || DOCKER_IMAGE=$1
-
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    "-h"|"--help")
+        usage
+        exit 0
+        ;;
+    "-c"|"--cached")
+        MOUNT_OPT=:cached
+        ;;
+    "-d"|"--delegated")
+        MOUNT_OPT=:delegated
+        ;;
+    *)
+        DOCKER_IMAGE=$@
+        break
+        ;;
+    esac
+    shift
+done
 
 if ! [[ $(docker images -q ${DOCKER_IMAGE}:latest) ]]; then
     docker build --tag=${DOCKER_IMAGE}:latest ${ROOT_DIR}
@@ -15,7 +42,7 @@ fi
 if [[ $(docker container ls -q -f name=${DOCKER_IMAGE}) ]]; then
     docker exec -it ${DOCKER_IMAGE} bash
 else
-    docker run --rm -it -v ${ROOT_DIR}:/workdir --workdir=/workdir --name=${DOCKER_IMAGE} \
+    docker run --rm -it -v ${ROOT_DIR}:/workdir${MOUNT_OPT} --workdir=/workdir --name=${DOCKER_IMAGE} \
         -p 8050:8050 \
         -p 8060:8060 \
         -p 8070:8070 \
